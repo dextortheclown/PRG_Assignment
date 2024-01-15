9e6dcd294682d7e9274f6c2f53fca4eb009478c3
dextortheclown <dexterwong0724@gmail.com>
2024-01-15 19:22:25 +0800

    updated classes

--- PRG_Assignment/Customer.cs
+++ PRG_Assignment/Customer.cs
@@ -2,6 +2,23 @@
 {
     internal class Customer
     {
+        public string name {  get; set; }
+        public int memberid {  get; set; }
+        public DateTime dob { get; set; }
+        public Order currentOrder { get; set; }
+        public List<Order> orderHistory { get; set; }
+        public PointCard rewards { get; set; }
+        public Customer() { }
+        public Customer(string name, int memberid, DateTime dob)
+        {
+            this.name = name;
+            this.memberid = memberid;
+            this.dob = dob;
+            orderHistory = new List<Order>();
+        }
+        public Order MakeOrder()
+        {
 
+        }
     }
 }
